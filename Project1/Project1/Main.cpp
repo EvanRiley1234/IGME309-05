0a3e845744971c67945b8b92ca2330b200013bd1
EvanRiley1234 <54547434+EvanRiley1234@users.noreply.github.com>
2022-09-16 00:35:31 -0400

    forgot too push oops

--- Project1/Project1/Main.cpp
+++ Project1/Project1/Main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include <cstdio>
 using namespace std;
+
 int main()
 {
+	//say hello world in the console app
 	cout << "Hello World!" << endl;
+
+	//ask the user to end the porgram
+	cout << "Enter any ket to end the program";
+	getchar();
 	return 0;
 }
